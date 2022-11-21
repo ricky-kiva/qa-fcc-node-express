b4f5979e03f032acec69eb81a4466fe6700ebc67
Rickyslash <85386584+ricky-kiva@users.noreply.github.com>
2022-11-21 09:27:51 +0000

    connecting to mongodb

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const session = require('express-session');
 const passport = require('passport')
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const { ObjectID } = require('mongodb')
 
 const app = express();
 
@@ -20,11 +21,36 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 })) // using express-session
-app.use(passport.initialize()) // use passport
-app.use(passport.session())
+app.use(passport.initialize()); // use passport
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.render('index', {title: 'Hello', message: 'Please log in'}) // render pug index on '/' GET
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users'); // connecting to database after connected to cluster via './connection' (myDB)
+
+  // Change HTML after database is connected
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+      done(null, doc); // looking for one new generated class of ObjectID
+    })
+  })
+})
+  .catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
