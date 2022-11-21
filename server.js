bd77782657ec3433fd900e9f22b2f7c70a422cb3
Rickyslash <85386584+ricky-kiva@users.noreply.github.com>
2022-11-21 08:40:48 +0000

    configure express-session and passport package

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
+const session = require('express-session');
+const passport = require('passport')
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -12,9 +14,17 @@ app.set('views', './views/pug') // set the views location of pug
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ 
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+})) // using express-session
+app.use(passport.initialize()) // use passport
+app.use(passport.session())
 
 app.get('/', (req, res) => {
-  res.render('index') // render pug index on '/' GET
+  res.render('index', {title: 'Hello', message: 'Please log in'}) // render pug index on '/' GET
 });
 
 const PORT = process.env.PORT || 3000;
